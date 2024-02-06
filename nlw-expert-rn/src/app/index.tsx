9ae2c85945b8fcf5471a8b348140c2c70d0b910e
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-02-06 11:28:23 -0300

    feat: rn create category button

--- nlw-expert-rn/src/app/index.tsx
+++ nlw-expert-rn/src/app/index.tsx
@@ -1,10 +1,17 @@
 import { View } from 'react-native';
 import { Header } from '@/components/header';
+import { CategoryButton } from '@/components/category-button';
 
 export default function Home() {
   return (
     <View className="flex-1 pt-4">
       <Header title="Faça seu pedido" cartQuantityItems={4} />
+
+      <View className="flex-row gap-4 justify-between">
+        <CategoryButton title="Lanche do dia" isSelected />
+        <CategoryButton title="Promoções" />
+        <CategoryButton title="Bebidas" />
+      </View>
     </View>
   );
 }
