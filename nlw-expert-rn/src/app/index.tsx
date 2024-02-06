0b70db9530914972f57d28f217b615d18891d736
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-02-06 11:41:45 -0300

    feat: rn use category button with flat list

--- nlw-expert-rn/src/app/index.tsx
+++ nlw-expert-rn/src/app/index.tsx
@@ -1,17 +1,35 @@
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { Header } from '@/components/header';
 import { CategoryButton } from '@/components/category-button';
+import { CATEGORIES } from '@/utils/data/products';
+import { useState } from 'react';
 
 export default function Home() {
+  const [category, setCategory] = useState<string>(CATEGORIES[0]);
+
+  function handleCategorySelected(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
     <View className="flex-1 pt-4">
       <Header title="Faça seu pedido" cartQuantityItems={4} />
 
-      <View className="flex-row gap-4 justify-between">
-        <CategoryButton title="Lanche do dia" isSelected />
-        <CategoryButton title="Promoções" />
-        <CategoryButton title="Bebidas" />
-      </View>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className="max-h-10 mt-5"
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   );
 }
