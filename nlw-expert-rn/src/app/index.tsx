b789664b308e065b21224aa64da53328ca1a7765
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-02-06 14:38:54 -0300

    feat: rn get cart quantity items in home screen

--- nlw-expert-rn/src/app/index.tsx
+++ nlw-expert-rn/src/app/index.tsx
@@ -5,10 +5,17 @@ import { Header } from '@/components/header';
 import { CategoryButton } from '@/components/category-button';
 import { Product } from '@/components/product';
 import { CATEGORIES, MENU } from '@/utils/data/products';
+import { useCartStore } from '@/stores/cart-store';
 
 export default function Home() {
+  const cartStore = useCartStore();
   const [category, setCategory] = useState<string>(CATEGORIES[0]);
   const sectionListRef = useRef<SectionList>(null);
+
+  const cartQuantityItems = cartStore.products.reduce(
+    (total, product) => total + product.quantity,
+    0
+  );
 
   function handleCategorySelected(selectedCategory: string) {
     setCategory(selectedCategory);
@@ -28,7 +35,7 @@ export default function Home() {
 
   return (
     <View className="flex-1 pt-4">
-      <Header title="Faça seu pedido" cartQuantityItems={4} />
+      <Header title="Faça seu pedido" cartQuantityItems={cartQuantityItems} />
 
       <FlatList
         data={CATEGORIES}
