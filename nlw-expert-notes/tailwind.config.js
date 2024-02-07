76c715a3cd363c8df14d5350c217235085e65a34
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-02-07 14:42:20 -0300

    feat: react config fonts and add assets

--- nlw-expert-notes/tailwind.config.js
+++ nlw-expert-notes/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
