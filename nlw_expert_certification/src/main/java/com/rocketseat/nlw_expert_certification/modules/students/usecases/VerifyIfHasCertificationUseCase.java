b7714a3d7bc030fbd8c14c2915a570b99d096a81
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-02-06 21:42:57 -0300

    feat: java use certification study repository in use case

--- nlw_expert_certification/src/main/java/com/rocketseat/nlw_expert_certification/modules/students/usecases/VerifyIfHasCertificationUseCase.java
+++ nlw_expert_certification/src/main/java/com/rocketseat/nlw_expert_certification/modules/students/usecases/VerifyIfHasCertificationUseCase.java
@@ -1,16 +1,23 @@
 package com.rocketseat.nlw_expert_certification.modules.students.usecases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rocketseat.nlw_expert_certification.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.nlw_expert_certification.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
+
+  @Autowired
+  private CertificationStudentRepository certificationStudentRepository;
+
   public boolean execute(VerifyHasCertificationDTO dto) {
-    if (dto.getEmail().equals("rudolfhiok@email.com") 
-        && dto.getTechnology().equals("JAVA")) {
-      return true;
+    var certifications = this.certificationStudentRepository
+      .findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+    if (certifications.isEmpty()) {
+      return false;
     }
-    return false;
+    return true;
   }
 }
