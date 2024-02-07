a3096db90d6d62684150a8119ed49e304778bf8c
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-02-07 11:57:09 -0300

    refactor: node separate prisma client lib and create pool route

--- nlw-expert-polls/src/http/server.ts
+++ nlw-expert-polls/src/http/server.ts
@@ -1,33 +1,15 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
-import { z } from 'zod';
+import { createPool } from './routes/create-poll';
 
 const PORT = 3333;
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
 app.get('/hello', () => {
   return 'Hello NLW';
 });
 
-app.post('/polls', async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
-  const { title } = createPollBody.parse(request.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  });
-
-  return reply.status(201).send({
-    pollId: poll.id,
-  });
-});
+app.register(createPool);
 
 app
   .listen({ port: PORT })
